b8f3a1c055322a6bd10993ac2aeb1f622ec58b9b
aristi1215 <juanarieda@gmail.com>
2024-02-16 13:49:20 -0500

    "ADD" estilos finales y segunda pagina(our skills)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ let burguer = document.getElementById("burguer");
 let mobileMenu = document.getElementById("mobile-menu");
 let closeMenu = document.getElementById("close-menu")
 let body = document.querySelector("body")
+let imgsContainers = document.getElementsByClassName("our-creations-container-desktop")
 
 burguer.onclick = () =>{
     mobileMenu.classList.remove("hidden")
@@ -14,4 +15,14 @@ closeMenu.onclick = () =>{
     mobileMenu.classList.remove("translate-x-[0]")
     mobileMenu.classList.add("translate-x-[100rem]")
     body.classList.remove("overflow-y-hidden")
+}
+
+for (let i = 0; i < imgsContainers.length; i++) {
+    imgsContainers[i].addEventListener("mouseover", function(){
+        imgsContainers[i].children[1].style.transform = "scale(1.2)";
+    });
+
+    imgsContainers[i].addEventListener("mouseout", function(){
+        imgsContainers[i].children[1].style.transform = "scale(1)";
+    });
 }
